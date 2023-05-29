3f85542de6bebbb19b42b4eb6d36b6fb67f695ec
anthony <denicad@msn.com>
2023-05-29 17:43:32 -0400

    trying to fix issue with starter accounts duplicating

--- src/main/java/banker/banker/bank/accountConfig.java
+++ src/main/java/banker/banker/bank/accountConfig.java
@@ -17,9 +17,9 @@ public class accountConfig {
     @Bean
     CommandLineRunner commandLineRunner(accountRepo repo,transactionsRepo transRepo){
         return  args -> {
-            account check=new account("checking", LocalDate.of(2022, OCTOBER,26),0);
-            account saving=new account("savings",LocalDate.of(2022,OCTOBER,26),0);
-            repo.saveAll(List.of(check,saving));
+//            account check=new account("checking", LocalDate.of(2022, OCTOBER,26),0);
+//            account saving=new account("savings",LocalDate.of(2022,OCTOBER,26),0);
+//            repo.saveAll(List.of(check,saving));
         };
     }
 }
